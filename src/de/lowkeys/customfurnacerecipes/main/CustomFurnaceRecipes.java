94b291d7fd6c43d04f069df25ff6e3409a3ae22c
Gero Lindner <gero.lindner@outlook.de>
2021-08-23 21:52:55 +0200

    Added errors.yml file for saving the error to a file

--- src/de/lowkeys/customfurnacerecipes/main/CustomFurnaceRecipes.java
+++ src/de/lowkeys/customfurnacerecipes/main/CustomFurnaceRecipes.java
@@ -2,13 +2,19 @@ package de.lowkeys.customfurnacerecipes.main;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class CustomFurnaceRecipes extends JavaPlugin {
+    File errorFile;
+    FileConfiguration errorConfiguration;
 
     @Override
     public void onEnable() {
@@ -19,6 +25,7 @@ public class CustomFurnaceRecipes extends JavaPlugin {
 
         Bukkit.addRecipe(furnaceRecipe);
 
+        generateErrorFile();
         addDefaultConfig();
         generateRecipes();
     }
@@ -53,8 +60,27 @@ public class CustomFurnaceRecipes extends JavaPlugin {
                 Bukkit.addRecipe(furnaceRecipe);
             } catch (Exception exception) {
                 System.out.println("Error at entry: " + source);
+                errorConfiguration.set(source, source);
+                try {
+                    errorConfiguration.save(errorFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
         }
+    }
+
+    public void generateErrorFile() {
+        errorFile = new File("plugins/CustomFurnaceRecipes", "errors.yml");
+        if(!errorFile.exists()) {
+            try {
+                errorFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        errorConfiguration = YamlConfiguration.loadConfiguration(errorFile);
     }
 }
